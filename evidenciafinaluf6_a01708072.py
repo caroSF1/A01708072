6ab1f93fbfd45e9ac9a0b73b434df56a76120b39
Carolina Solis Flores <101727710+caroSF1@users.noreply.github.com>
2023-06-05 19:16:23 -0600

    Add files via upload

--- evidenciafinaluf6_a01708072.py
+++ evidenciafinaluf6_a01708072.py
@@ -9,9 +9,12 @@ Original file is located at
 
 import streamlit as st
 import pandas as pd
+import numpy as np
 st.title('Police Incident Reports from 2018 to 2020 in San Francisco')
 df = pd.read_csv("https://drive.google.com/drive/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ")
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
-mapa=pd.DataFrame()
+mapa=pd.DataFrame(
+    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+    columns=['lat', 'lon'])
 mapa = mapa.dropna()
 st.map(mapa.astype(int))
