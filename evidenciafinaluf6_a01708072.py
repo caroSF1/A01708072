88b60d659be0e6903d8a6441db95ccb56bb224c3
Carolina Solis Flores <101727710+caroSF1@users.noreply.github.com>
2023-06-05 18:50:43 -0600

    Add files via upload

--- evidenciafinaluf6_a01708072.py
+++ evidenciafinaluf6_a01708072.py
@@ -15,7 +15,7 @@ import plotly.figure_factory as ff
 from bokeh.plotting import figure
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("Police_Department_Incident_Reports__2018_to_Present.csv")
+df = pd.read_csv("https://drive.google.com/drive/folders/1Hd2HOFknGccrm11VSdDp5N9-ADkboWvZ")
 
 st.markdown('The data shown below belongs to incident reports in the city of San Francisco, from the year 2018 to 2020, with details from each case such as date, day of the week, police district, neighborhood in which it happened, type of incident in category and subcategory, exact location and resolution.')
 
